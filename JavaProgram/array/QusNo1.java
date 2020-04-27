538833dd6edd5f4fc183875e14f0721622adb3e0
Gopal <gopalkumar211@gmail.com>
2020-04-27 18:44:44 +0530

    Run Program

--- JavaProgram/array/QusNo1.java
+++ JavaProgram/array/QusNo1.java
@@ -1,11 +1,12 @@
-// Write a program to find the index of an array element.
+//QusNo.1 Write a program to find the index of an array element.
 import java.util.Scanner;
 class QusNo1{
+public static void main(String[] arg){
 	int size;
 	Scanner sc = new Scanner(System.in);
 	System.out.println("Enter size of array");
 	size = sc.nextInt();
-	int[] arr = int[size];
+	int[] arr = new int[size];
 	System.out.println("Enter Element of Array");
 	for(int i =0;i<size;i++)
 	{
@@ -15,5 +16,5 @@ class QusNo1{
 	{
 		System.out.println("Index of "+ i +": "+arr[i]);
 	}
-
+}
 }
